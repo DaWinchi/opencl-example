2c5058ba031d7d0d7aef9b1878ddfcdcc4b57fba
prutskov <lehaprutskov@gmail.com>
2018-11-09 02:11:33 +0300

    Added opencl function call

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
+#define __CL_ENABLE_EXCEPTIONS
+
 #include <iostream>
+#include <CL/cl.hpp>
 
 int main(int argc, char ** argv)
 {
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    for(int i =0; i<platforms.size(); i++)
+    {
+        std::vector<cl::Device> devices;
+        platforms[i].getDevices(CL_DEVICE_TYPE_ALL, &devices);
+        
+        for (int j = 0; j <devices.size(); j++)
+        {
+            std::cout<<devices[j].getInfo<CL_DEVICE_NAME>()<<std::endl;
+        }
+    }
     std::cout<<"Hello world"<<std::endl;
     return 0;
 }
